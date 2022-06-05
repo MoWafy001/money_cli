d33945ec3f78e1bc2a9affb9af5fc7bece3f2c5f
mowafy001 <wafy123445@gmail.com>
2022-06-05 18:46:23 +0200

    init

---
+++ src/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+DATABASE_URI = os.environ['DATABASE_URI']
---
+++ src/models.py
@@ -0,0 +1,58 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from .config import *
+
+
+# initialize
+engine = create_engine(DATABASE_URI, echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# models
+
+
+class History(Base):
+  __tablename__ = 'history'
+
+  date = Column(DateTime, primary_key=True)
+  value = Column(Float, nullable=False)
+
+  users = relationship('User', secondary='user_history', back_populates='history')
+
+
+class User(Base):
+  __tablename__ = 'users'
+
+  username = Column(String, primary_key=True)
+  total = Column(Integer, default=0, nullable=False)
+
+  history = relationship('History', secondary='user_history', back_populates='users')
+
+
+  def add_or_spend(self, value, when):
+    old_total = self.total
+
+    try:
+      h = History(date=when, value=value)
+      self.history.append(h)
+
+      if self.total is None:
+        self.total = 0
+        
+      self.total += value
+    except Exception as e:
+      self.total = old_total
+      raise e
+
+
+class UserHistory(Base):
+  __tablename__ = 'user_history'
+
+  username = Column(String, ForeignKey('users.username'), primary_key=True)
+  date = Column(DateTime, ForeignKey('history.date'), primary_key=True)
+
+
+Base.metadata.create_all(engine)
