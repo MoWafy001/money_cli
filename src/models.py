45fc225329c0866c55021fcd1da455faa0c66208
mowafy001 <wafy123445@gmail.com>
2022-06-05 18:58:28 +0200

    only import DATABASE_URI

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
-from .config import *
+from .config import DATABASE_URI 
 
 
 # initialize
@@ -12,8 +12,6 @@ session = Session()
 
 
 # models
-
-
 class History(Base):
   __tablename__ = 'history'
 
