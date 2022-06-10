c32b45e5b45e143733856c1e1543099ea00fbe12
mowafy001 <wafy123445@gmail.com>
2022-06-10 02:24:57 +0200

    move methods to a seperate file

--- src/app.py
+++ src/app.py
@@ -1,49 +1,24 @@
-from .models import *
+from .models import User, session
 from .config import CURRENT_USER
-from datetime import datetime
+from .methods import Methods
 
 
 current_user = session.query(User).get(CURRENT_USER)
-
-
-def get_total():
-    print('Total:', current_user.total)
-
-
-def set_total(new_total):
-    current_user.total = int(new_total)
-    session.commit()
-    get_total()
-
-
-def add(value):
-    value = int(value)
-    current_user.add_or_spend(value, datetime.now())
-    session.commit()
-    print(value, 'added')
-    get_total()
-
-
-def spend(value):
-    value = int(value)
-    current_user.add_or_spend(-value, datetime.now())
-    session.commit()
-    print(value, 'spent')
-    get_total()
+methods = Methods(current_user, session)
 
 
 commands = {
-    'total': get_total,
-    'set-total': set_total,
-    'add': add,
-    'spend': spend
+    'total': methods.get_total,
+    'set-total': methods.set_total,
+    'add': methods.add,
+    'spend': methods.spend
 }
 
 
 def handel_command(command):
     try:
         if len(command) == 0:
-            get_total()
+            methods.get_total()
         elif len(command) > 1:
             commands[command[0]](command[1])
         else:
---
+++ src/methods.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+
+class Methods:
+    def __init__(self, current_user, session):
+        self.current_user = current_user
+        self.session = session
+
+
+    def get_total(self):
+        print('Total:', self.current_user.total)
+
+
+    def set_total(self, new_total):
+        self.current_user.total = int(new_total)
+        self.session.commit()
+        self.get_total()
+
+
+    def add(self, value):
+        value = int(value)
+        self.current_user.add_or_spend(value, datetime.now())
+        self.session.commit()
+        print(value, 'added')
+        self.get_total()
+
+
+    def spend(self, value):
+        value = int(value)
+        self.current_user.add_or_spend(-value, datetime.now())
+        self.session.commit()
+        print(value, 'spent')
+        self.get_total()
